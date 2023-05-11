05c8980247bccf5cfe1183cb8c56c1f898fe8ae8
Maria <achigarpia@gmail>
2023-05-11 19:19:17 -0300

    validaciones de algunos campos

--- almacen/src/pages/RegisterForm.jsx
+++ almacen/src/pages/RegisterForm.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import {
   FormControl,
@@ -6,17 +6,21 @@ import {
   Input,
   Container,
   Button,
+  Box,
+  Text
 } from "@chakra-ui/react";
 
 const RegisterForm = () => {
   const navigate = useNavigate(); // Obtener el objeto de historial
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [passwordsMatch, setPasswordsMatch] = useState(true);
   const [role, setRole] = useState("");
   const [showPassword, setShowPassword] = useState(false);
+  const [isFormValid, setIsFormValid] = useState(false);
 
   const handleRegister = async () => {
     try {
@@ -25,7 +29,7 @@ const RegisterForm = () => {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ name, email, password, role: "admin" }),
+        body: JSON.stringify({ name, email, password, role: role ?? "admin" }),
       });
 
       const data = await response.json();
@@ -43,7 +47,14 @@ const RegisterForm = () => {
       console.error("Error al registrar el usuario:", error);
     }
   };
-
+  const validateEmail = () => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!regex.test(email)) {
+      setEmailError("Ingrese un email válido. Ej: pepe@gmail.com");
+    } else {
+      setEmailError("");
+    }
+  };
   const handleTogglePassword = () => {
     setShowPassword(!showPassword);
   };
@@ -56,65 +67,78 @@ const RegisterForm = () => {
     setConfirmPassword(e.target.value);
     setPasswordsMatch(e.target.value === password);
   };
+  const validateForm = () => {
+    setIsFormValid(
+      name !== "" &&
+        email !== "" &&
+        password !== "" &&
+        confirmPassword !== "" &&
+        passwordsMatch
+    );
+  };
+  useEffect(() => {
+    validateForm();
+  }, [name, email, password, confirmPassword, passwordsMatch]);
 
   return (
     <>
       <Container maxW="md" maxH="md" direction={"colum"} placeItems="center">
         <form>
-          <FormControl id="name" marginBottom={10} isRequired>
-            <FormLabel>Usuario</FormLabel>
-            <Input
-              type="name"
-              placeholder="Ingresa tu nombre"
-              autoComplete="off"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </FormControl>
-          <FormControl id="email" marginBottom={10} isRequired>
-            <FormLabel>Email</FormLabel>
-            <Input
-              type="email"
-              placeholder="Ingresa tu email"
-              autoComplete="off"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </FormControl>
-          <FormControl id="password" marginBottom={10} isRequired>
-            <FormLabel>Contraseña</FormLabel>
-            <Input
-              type={showPassword ? "text" : "password"}
-              placeholder="**********"
-              value={password}
-              onChange={handlePasswordChange}
-            />
-            <button type="button" onClick={handleTogglePassword}>
-              {showPassword ? "Ocultar" : "Mostrar"}
-            </button>
-          </FormControl>
-          <FormControl id="password2" marginBottom={10} isRequired>
-            <FormLabel>Vuelva a escribir su Contraseña</FormLabel>
-            <Input
-              type={showPassword ? "text" : "password"}
-              placeholder="**********"
-              value={password}
-              onChange={handleConfirmPasswordChange}
-            />
-            <button type="button" onClick={handleTogglePassword}>
-              {showPassword ? "Ocultar" : "Mostrar"}
-            </button>
-          </FormControl>
-          {passwordsMatch ? (
-            <p>Las contraseñas coinciden.</p>
-          ) : (
-            <p>Las contraseñas no coinciden.</p>
-          )}
+          <Box p={10}>
+            <FormControl id="name" marginBottom={10} isRequired>
+              <FormLabel>Usuario</FormLabel>
+              <Input
+                type="name"
+                autoComplete="off"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </FormControl>
+            <FormControl id="email" marginBottom={10} isRequired>
+              <FormLabel>Email</FormLabel>
+              <Input
+                type="email"
+                autoComplete="off"
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                  validateEmail();
+                }}
+              />
+              {emailError && <Text color="red">{emailError}</Text>}
+            </FormControl>
+            <FormControl id="password" marginBottom={10} isRequired>
+              <FormLabel>Contraseña</FormLabel>
+              <Input
+                type={showPassword ? "text" : "password"}
+                value={password}
+                onChange={handlePasswordChange}
+              />
+              <button type="button" onClick={handleTogglePassword}>
+                {showPassword ? "Ocultar" : "Mostrar"}
+              </button>
+            </FormControl>
+            <FormControl id="password2" marginBottom={10} isRequired>
+              <FormLabel>Vuelva a escribir su Contraseña</FormLabel>
+              <Input
+                type={showPassword ? "text" : "password"}
+                value={confirmPassword}
+                onChange={handleConfirmPasswordChange}
+              />
+              <button type="button" onClick={handleTogglePassword}>
+                {showPassword ? "Ocultar" : "Mostrar"}
+              </button>
+              {!passwordsMatch && <Text color="red">Las contraseñas NO coinciden.</Text>}
+            </FormControl>
+            
+          </Box>
+
           <Button
             type="button"
             onClick={handleRegister}
             backgroundColor="green"
             textColor="white"
+            isDisabled={!isFormValid}
           >
             Registrarse
           </Button>
