1fd4ec4f24700cece3211c90df75224f836f8966
Maria <achigarpia@gmail>
2023-05-25 00:20:44 -0300

    zxing - lector de codigo de barras

--- almacen/src/pages/Carga.jsx
+++ almacen/src/pages/Carga.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState, useRef } from "react";
 import { NavLink } from "react-router-dom";
-import Quagga from "quagga";
-//https://github.com/ericblade/quagga2
+import { BrowserMultiFormatReader } from "@zxing/library";
 import {
   Heading,
   Grid,
@@ -16,77 +15,46 @@ const Carga = () => {
   const scannerContainerRef = useRef(null);
   const [code, setCode] = useState("");
 
-  const onDetected = (result) => {
-    //console.log(result.target.value)
-    console.log(result.codeResult.code);
-    setCode(result.codeResult.code); // Captura el código de barras detectado y lo guarda en el estado 'code'
+  const videoRef = useRef(null);
+  const reader = useRef(new BrowserMultiFormatReader());
+
+  const prepareVideoElement = (videoElement) => {
+    if (!videoElement) {
+      return;
+    }
+
+    // Verificar si el objeto videoElement tiene la propiedad setAttribute antes de usarla
+    if (typeof videoElement.setAttribute === "function") {
+      videoElement.setAttribute("autoplay", true);
+      videoElement.setAttribute("muted", true);
+      videoElement.setAttribute("playsinline", true);
+    }
   };
-  // const handleCapture =(e)=>{
-  //   console.log(e)
-  // }
-  //Para el Código de Barra
   useEffect(() => {
-    Quagga.init(
+    console.log(code);
+    console.log(videoRef.current.value);
+    if (!videoRef.current) {
+      return;
+    }
+    prepareVideoElement(videoRef.current);
+    if (!videoRef.current) return;
+    reader.current.decodeFromConstraints(
       {
-        inputStream: {
-          name: "Live",
-          type: "LiveStream", //ImageStream, VideoStream, or LiveStream
-          target: scannerContainerRef.current,
-          constraints: {
-            width: 440,
-            height: 250,
-            facingMode: "environment", // Usar la cámara trasera del dispositivo
-          },
+        audio: false,
+        video: {
+          facingMode: "environment",
         },
-        area: {
-          // defines rectangle of the detection/localization area
-          top: "0%", // top offset
-          right: "0%", // right offset
-          left: "0%", // left offset
-          bottom: "0%", // bottom offset
-        },
-        singleChannel: false, // true: only the red color-channel is read
-        // decoder: {
-        //   readers: ["ean_5_reader", "ean_2_reader"], // Tipo de código de barras que se va a leer (EAN) - ,"code_128_reader"(default),"code_39_reader"
-        //   multiple: false,
-        // },
-        decoder: {
-          readers: [{
-              format: "ean_reader",//"ean_reader"
-              config: {
-                  supplements: [
-                      'ean_5_reader', 'ean_2_reader',"code_128_reader"
-                  ]
-              }
-          }]
       },
-        locator: {
-          patchSize: "medium", // Tamaño del parche utilizado para la detección del código de barras (puede ser "small", "medium" o "large")
-          halfSample: true, // Mejora la detección en imágenes más grandes
-        },
-        locate: true, // false - Esto evitará la superposición visual de la detección del código de barras en la interfaz.
-        src: '/test/fixtures/code_128/image-001.jpg' // or 'data:image/jpg;base64,' + data
-      },
-      (err) => {
-        if (err) {
-          console.error("Error al inicializar Quagga:", err);
-          return;
-        }
-        console.log("Initialization finished. Ready to start");
-        Quagga.start();
+      videoRef.current,
+      (result, error) => {
+        if (result) console.log(result);
+        if (error) console.log(error);
       }
     );
-
-    Quagga.onDetected(onDetected);
-
     return () => {
-      Quagga.offDetected(onDetected);
+      reader.current.reset();
     };
-    // Cleanup: Detener el escáner cuando el componente se desmonta
-    // return () => {
-    //   Quagga.stop();
-    // };
-  }, []);
+  }, [videoRef]);
 
   return (
     <>
@@ -102,12 +70,20 @@ const Carga = () => {
           <form>
             <FormControl>
               <FormLabel>Código</FormLabel>
-              <Box height={500} ref={scannerContainerRef}>
+              <Box height={50} ref={videoRef}>
                 {/* Me falta decodificar ese "code" */}
-                <Input type="text" name="code" value={code} id="codeBarra" />
+                <Input type="text" name="code" onChange={()=> setCode()} />
               </Box>
 
               <FormLabel>Nombre Producto</FormLabel>
+              <Input type="text" name="code" />
+              <FormLabel>Cantidad</FormLabel>
+              <Input type="text" name="code" />
+              <FormLabel>Fecha de vencimiento</FormLabel>
+              <Input type="text" name="code" />
+              <FormLabel>Descripción</FormLabel>
+              <Input type="text" name="code" />
+              <FormLabel>Imagen</FormLabel>
               <Input type="text" name="code" />
             </FormControl>
           </form>
