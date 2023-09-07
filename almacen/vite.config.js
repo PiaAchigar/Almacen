009cc10e99175dcaf7465530c43412932209c135
Maria <achigarpia@gmail>
2023-09-07 13:41:33 -0300

    deploy with gh-pages

--- almacen/vite.config.js
+++ almacen/vite.config.js
@@ -1,12 +1,27 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
+const root = resolve(__dirname, "src");
+const outDir = resolve(__dirname, "dist");
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/almacen/",
+  root,
   plugins: [react()],
   server: {
     watch: {
       usePolling: true,
     },
   },
+  build: {
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, "index.html"),
+      },
+    },
+  },
+  assetsDir: "img",
 });
