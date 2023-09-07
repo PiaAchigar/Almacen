d6676a64461349b924cb14c732077b42ba190c1f
Maria <achigarpia@gmail>
2023-09-07 14:25:16 -0300

    deploy 5

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { resolve } from "path";
-const root = resolve(__dirname, "src");
 const outDir = resolve(__dirname, "dist");
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/almacen/",
-  root,
   plugins: [react()],
   server: {
     watch: {
@@ -19,7 +16,7 @@ export default defineConfig({
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: resolve(root, "index.html"),
+        main: "index.html",
       },
     },
   },
