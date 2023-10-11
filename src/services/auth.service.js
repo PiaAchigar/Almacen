b5fd3e3a7798d06c71d93a02bfb13202d7985c12
Maria <achigarpia@gmail>
2023-10-10 22:33:24 -0300

    IP render for server

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,9 @@
 //Es para la Autenticación del Usuario
 export async function loginService(email, password) {
-  const response = await fetch("http://localhost:3001/login", {
+//   35.160.120.126
+// 44.233.151.27
+// 34.211.200.85
+  const response = await fetch("http://35.160.120.126/login", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
